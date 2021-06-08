da439e263bff35b963fc93508ee1dec8fc7e24e9
palemoons <974473140@qq.com>
2021-06-08 20:06:30 +0800

    replaced icons && added content switching

--- src/Pages/Console.js
+++ src/Pages/Console.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
@@ -13,12 +13,15 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import HomeIcon from '@material-ui/icons/Home';
+import AddIcon from '@material-ui/icons/Add';
+import ImportExportIcon from '@material-ui/icons/ImportExport';
+import SearchIcon from '@material-ui/icons/Search';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -54,6 +57,11 @@ const useStyles = makeStyles((theme) => ({
             duration: theme.transitions.duration.enteringScreen,
         }),
     },
+    menuItem: {
+        display: 'flex',
+
+    }
+    ,
     menuButton: {
         marginRight: 36,
     },
@@ -100,13 +108,13 @@ const useStyles = makeStyles((theme) => ({
 
 function Console() {
     const classes = useStyles();
-    const theme = useTheme();
     const anchorRef = useRef(null);
     const [userOpen, setUserOpen] = useState(false);
     const prevOpen = useRef(userOpen);
     const [open, setOpen] = useState(false);
     const [loginStatus, setLoginStatus] = useState(false);
     const [user, setUser] = useState('');
+    const [menuIndex, setMenuIndex] = useState(0);
 
     useEffect(() => {
         if (prevOpen.current === true && userOpen === false) {
@@ -170,6 +178,16 @@ function Console() {
             })
             .catch(() => alert('与服务器连接时发生错误'))
     }
+
+    const indexSwitch = (index) => {
+        switch (index) {
+            case 0: return <ImportBook />;
+            case 1: return <SearchBook />;
+            case 2: return <ManageBook />;
+            case 3: return <ManageCard />;
+            default: alert('index处于未知状态，请联系管理员'); return;
+        }
+    }
     return (
         <div className={classes.root}>
             <CssBaseline />
@@ -232,11 +250,16 @@ function Console() {
                                     <MenuItem>
                                         <Link
                                             to='/'
+                                            className={classes.menuItem}
                                             style={{ color: 'inherit' }}>
+                                            <HomeIcon />
                                             Homepage
                                 </Link>
                                     </MenuItem>
-                                    <MenuItem onClick={handleLogout}>
+                                    <MenuItem
+                                        onClick={handleLogout}
+                                        className={classes.menuItem}>
+                                        <ExitToAppIcon />
                                         Logout
                                 </MenuItem>
                                 </MenuList>
@@ -260,55 +283,73 @@ function Console() {
             >
                 <div className={classes.toolbar}>
                     <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        <ChevronLeftIcon />
                     </IconButton>
                 </div>
                 <Divider />
                 <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
+                    <ListItem button key='图书入库' onClick={() => { setMenuIndex(0) }}>
+                        <ListItemIcon>
+                            <AddIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='图书入库' />
+                    </ListItem>
+                    <ListItem button key='图书查询'>
+                        <ListItemIcon>
+                            <SearchIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='图书查询' />
+                    </ListItem>
+                    <ListItem button key='借还书籍'>
+                        <ListItemIcon>
+                            <ImportExportIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='借还书籍' />
+                    </ListItem>
+                    <ListItem button key='借书证管理'>
+                        <ListItemIcon>
+                            <AssignmentIndIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='借书证管理' />
+                    </ListItem>
                 </List>
             </Drawer>
             <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-        </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+                {
+                    indexSwitch(menuIndex)
+                }
             </main>
         </div>
+    )
+}
+
+function ImportBook() {
+    return (
+        <>
+        <div>hello world.</div>
+        </>
+    )
+}
+
+function SearchBook() {
+    return (
+        <>
+        </>
+    )
+}
+
+function ManageBook() {
+    return (
+        <>
+        </>
+    )
+}
+
+function ManageCard() {
+    return (
+        <>
+        </>
     )
 }
 export default Console;
