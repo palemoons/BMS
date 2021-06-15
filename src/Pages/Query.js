10bcac4f038677a8294b7383b39f81b275a4f2f0
palemoons <974473140@qq.com>
2021-06-15 23:29:53 +0800

    added query page

--- src/Pages/Query.js
+++ src/Pages/Query.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import BookmarksRoundedIcon from '@material-ui/icons/BookmarksRounded';
 
-function Query(){
-    
+import SearchBook from '../Components/SearchBook';
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center',
+    },
+}));
+
+function Query() {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <Link to="/">
+                        <IconButton edge="start" className={classes.icon} style={{ color: 'white' }}>
+                            <BookmarksRoundedIcon />
+                        </IconButton>
+                    </Link>
+                    <Typography variant="h5" className={classes.title}>
+                        Book Management System
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <SearchBook />
+        </>
+    )
 }
 
 export default Query;
